0033caf0cddaf2d5cd7a0e17d8da373d815ad796
Ziad Elsayed <ZiadElsayedAsr@gmail.com>
2025-02-27 21:46:04 +0200

    update

--- src/pages/Playgrounds.jsx
+++ src/pages/Playgrounds.jsx
@@ -1,5 +1,5 @@
 import DataTable from "react-data-table-component";
-import SecondTopbar from "../components/secondTopbar/secondTopbar";
+import SecondTopbar from "../components/secondTopbar/SecondTopbar";
 import Map from "../components/map/Map";
 
 const playgrounds = [
