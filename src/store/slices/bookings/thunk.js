b9ea8f90d8dd479a60d6a66e63b598a59b806f5f
Ziad Elsayed <123997217+Ziad-Asr@users.noreply.github.com>
2025-06-11 06:53:11 +0300

    update

--- src/store/slices/bookings/thunk.js
+++ src/store/slices/bookings/thunk.js
@@ -1,17 +1,19 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { useGetData } from "../../../hooks/useGetData";
 import { toast } from "react-toastify";
+
+import axios from "axios";
 
 export const fetchBookings = createAsyncThunk(
   "bookings/fetchBookings",
   async ({ courtId, date }, { rejectWithValue }) => {
     try {
-      const response = await useGetData(`/Booking/${courtId}/${date}`);
+      // const response = await useGetData(`/Booking/${courtId}/${date}`);
 
-      console.log("response");
-      console.log(response);
+      const response = await axios.get(
+        `http://localhost:5000/api/Booking/${courtId}/${date}`
+      );
 
-      return response;
+      return response?.data;
     } catch (error) {
       let errorMessage = "An error occurred while fetching bookings.";
       if (error.message?.includes("Network Error")) {
